e98d53552dbdb5152e977dab11c79ffc54b2e82b
Suilan Maia <suilansmaia@gmail.com>
2020-06-06 16:15:47 -0300

    Codejam Google challenges reorganized

---
+++ codejam-google/2008/problem-a_saving-the-universe/index.js
@@ -0,0 +1,24 @@
+/**
+ * Saving the universe - solution 
+ */
+
+// The readline is default in node engine
+// https://nodejs.org/api/readline.html
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name ? ", function(name) {
+    rl.question("Where do you live ? ", function(country) {
+        console.log(`${name}, is a citizen of ${country}`);
+        rl.close();
+    });
+});
+
+rl.on("close", function() {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
