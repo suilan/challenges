d0f32a7d11bb56c720435a07601778eabaf823ad
Suilan Maia <suilansmaia@gmail.com>
2020-11-10 08:52:38 -0300

    Include new challenge and remove renamed folders and files

---
+++ hackerrank/interview_preparation_kit/warm_ups/sock_merchant/js/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    var map = new Map();
+    var count=0;
+
+    for(var i=0; i<n; i++){
+        var total = map.get(ar[i]);
+        if( !total ) map.set(ar[i],1);
+        else map.set(ar[i], ++total);
+        
+        if(total%2==0) count++;
+    }
+
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
